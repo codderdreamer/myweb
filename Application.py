cb2e626ac8e8ea3761caa70f18e5f3ff32b2124b
codderdreamer <sevdayucedagg@gmail.com>
2023-05-09 15:18:01 +0300

    web site

---
+++ Application.py
@@ -0,0 +1,28 @@
+from Modules.FlaskModule import *
+from Modules.WebsocketModule import *
+import time
+
+
+BootError=False
+
+class Application():
+    def __init__(self):
+
+        # Flaskı Başlat
+        self.flask_module = FlaskModule(__name__)
+        self.flask_module.run(BootError)
+
+        # Websocketi Başlat
+        self.websocket_module = WebsocketModule(self)
+        self.websocket_module.run()
+
+Application()
+while True:
+    time.sleep(10)
+    if BootError:
+        print("BootError")
+        time.sleep(10)
+        break
+
+
+
