5b8f3074b15d8b23f3c60e05472cae48380c3b3e
codderdreamer <sevdayucedagg@gmail.com>
2023-05-29 10:15:31 +0300

    dene

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
-from Modules.FlaskModule import *
-from Modules.WebsocketModule import *
-import time
+# from Modules.FlaskModule import *
+# from Modules.WebsocketModule import *
+# import time
 
 
-BootError=False
+# BootError=False
 
-class Application():
-    def __init__(self):
+# class Application():
+#     def __init__(self):
 
-        # Flaskı Başlat
-        self.flask_module = FlaskModule(__name__)
-        self.flask_module.run(BootError)
+#         # Flaskı Başlat
+#         self.flask_module = FlaskModule(__name__)
+#         self.flask_module.run(BootError)
 
-        # # Websocketi Başlat
-        # self.websocket_module = WebsocketModule(self)
-        # self.websocket_module.run()
+#         # # Websocketi Başlat
+#         # self.websocket_module = WebsocketModule(self)
+#         # self.websocket_module.run()
 
-Application()
-while True:
-    time.sleep(10)
-    if BootError:
-        print("BootError")
-        time.sleep(10)
-        break
+# Application()
+# while True:
+#     time.sleep(10)
+#     if BootError:
+#         print("BootError")
+#         time.sleep(10)
+#         break
+
+from flask import Flask
+
+app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return "Hello, world!"
